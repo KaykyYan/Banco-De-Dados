1b74d7841223eee902a418270b9074416c0961fd
Kayky Yan <129062656+KaykyYan@users.noreply.github.com>
2023-06-05 23:19:02 -0300

    Add files via upload

---
+++ exercicio5.sql
@@ -0,0 +1,6 @@
+-- Inserindo dados em tabela
+
+use cursos;
+
+insert into tb_cursos (Id_Curso, Imagem_Curso, Data_Cadastro, Resumo, Nome_Curso, Investimento, Carga_Horaria)
+values (1, 'curso_angular.jpg', '2018-07-17 12:25:35', 'Aprenda a criar aplicações front-end incríveis com JavaScript, TypeScript e Angular', 'Web Completo com JS, TS E Angular', 575.85, 2);
